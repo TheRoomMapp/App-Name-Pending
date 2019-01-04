9fb097c77311cf2155f007a8a73e3ec6cb3b56d1
Cyndy <cyndy_nguyen@hotmail.com>
2019-01-03 17:25:57 -0800

    created function that can be used to pull departments, course numbers, section numbers, and course schedules

--- Dictionary Version.py
+++ Dictionary Version.py
@@ -18,23 +18,27 @@ def url_is_alive(url):
        return True
    except urllib.request.HTTPError:
        return False
-
-def get_values_from_ldict(unrefined_ldict, old_key, current_dict, new_key):
+   
+def create_new_ldict(old_ldict, old_key, new_key):
     '''
-    Function to extract required values from an unrefined list of dictionaries and adds them to an existing dictionary
-    @param unrefined_ldict - list of dictionaries to extract from
-    @param old_key - key in ldict corresponding to value of interest
-    @current_dict - dictionary the new_key:value will be added to
-    @new_key - key name chosen to correspond with value in new dictionary
+    Function to build upon existing list of dictionary (eg. adds course numbers to ldict_dept, adds sections to ldict_dept_course)
+    @param old_ldict - current list of dictionary to be built upon
+    @param old_key - key in SFU's json corresponding to value of interest
+    @param new_key - key name chosen to correspond with value in new dictionaries
     '''
     new_ldict = []
-    for item in unrefined_ldict:
-        new_dict = {}
-        new_dict.update(current_dict)
-        new_dict[new_key] = item[old_key]
-        new_ldict.append(new_dict)
+    for dictionary in old_ldict:
+        webname = SFU_DATA_WEBSITE
+        for key in dictionary:
+            webname += dictionary[key] +'/'
+        if url_is_alive(webname):
+            unrefined_ldict = get_data_from_url(webname)
+            for item in unrefined_ldict:
+                new_dict = {}
+                new_dict.update(dictionary)
+                new_dict[new_key] = item[old_key]
+                new_ldict.append(new_dict)
     return new_ldict
-        
 
 import urllib.request
 import json
@@ -44,21 +48,11 @@ CURRENT_YEAR = "2018"
 CURRENT_SEASON = "fall"
 SFU_DATA_WEBSITE = "http://www.sfu.ca/bin/wcm/course-outlines?"+CURRENT_YEAR+"/"+CURRENT_SEASON+"/"
 
-ldict_dept = get_values_from_ldict(get_data_from_url(SFU_DATA_WEBSITE), "text", {}, "department")
+ldict_dept = create_new_ldict([{}], 'text', 'department')
 
-ldict_dept_course = []
-for dictionary in ldict_dept:
-    webname = SFU_DATA_WEBSITE + dictionary['department'] + '/'
-    if url_is_alive(webname):
-        unrefined_list = get_data_from_url(webname)
-        ldict_dept_course += get_values_from_ldict(unrefined_list, 'text', dictionary, 'course')
+ldict_dept_course = create_new_ldict(ldict_dept, 'text', 'course')
 
-ldict_dept_course_sect = []
-for dictionary in ldict_dept_course:
-    webname = SFU_DATA_WEBSITE + dictionary['department'] + '/' + dictionary['course'] + '/'
-    if url_is_alive(webname):
-        unrefined_list = get_data_from_url(webname)
-        ldict_dept_course_sect += get_values_from_ldict(unrefined_list, 'text', dictionary, 'sect')
+ldict_dept_course_sect = create_new_ldict(ldict_dept_course, 'text', 'section')
 
-    
+   
     
